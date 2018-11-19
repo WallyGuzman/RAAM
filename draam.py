4f087e4e512b1d743c8279ed40377ceed53715e2
Atomkovich <Alexander.Tomkovich@utdallas.edu>
2018-11-19 13:52:00 -0600

    Fixed treatment of caps in embedding processing

--- draam.py
+++ draam.py
@@ -3,11 +3,14 @@ import torch.nn as nn
 import argparse
 import nltk
 
+VEC_DIM = 300
+
 def parse_args():
     parser = argparse.ArgumentParser(description='main.py')
 
     parser.add_argument('--lr', type=float, default=.001, help='learning rate')
     parser.add_argument('--training_file', type=str, default='train_data.txt', help='raw training data')
+    parser.add_argument('--vec_file', type=str, default='data/wiki-news-300d-1M.vec', help='vector file')
 
     args = parser.parse_args()
     return args
@@ -22,6 +25,26 @@ def get_vecs_from_sentence(sentence, word_dict):
             return None
         temp.append(curr)
     return np.array(temp)
+
+def parse_word_vecs(vec_path, vec_dim):
+    i = 1
+    dict = {}
+    in_file = open(vec_path)
+
+    next(in_file)
+    print(dict.keys())
+    for line in in_file:
+        parsed = line.split(' ', 1)
+        vec = np.fromstring(parsed[1], dtype = float, count = vec_dim, sep = " ")
+        dict[parsed[0]] = vec
+        i += 1
+
+        if i % 100000 == 0:
+            break
+
+    in_file.close()
+
+    return dict
 
 def parse_data(data):
     in_file = open(data)
@@ -38,4 +61,5 @@ if __name__ == "__main__":
     args = parse_args()
 
     sentences = parse_data(args.training_file)
-    create_vectors(sentences)
+    dict = parse_word_vecs(args.vec_file, VEC_DIM)
+    print(len(list(dict.keys())))
