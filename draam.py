72e026fc55eb9ac080af51f0981ca04d7d63deba
Atomkovich <Alexander.Tomkovich@utdallas.edu>
2018-11-19 14:09:28 -0600

    Finished preprocessing

--- draam.py
+++ draam.py
@@ -2,8 +2,7 @@ import numpy as np
 import torch.nn as nn
 import argparse
 import nltk
-
-VEC_DIM = 300
+import re
 
 def parse_args():
     parser = argparse.ArgumentParser(description='main.py')
@@ -11,15 +10,26 @@ def parse_args():
     parser.add_argument('--lr', type=float, default=.001, help='learning rate')
     parser.add_argument('--training_file', type=str, default='train_data.txt', help='raw training data')
     parser.add_argument('--vec_file', type=str, default='data/wiki-news-300d-1M.vec', help='vector file')
+    parser.add_argument('--vec_dim', type=int, default='300', help='dimensions of vector embeddings')
 
     args = parser.parse_args()
     return args
+
+def generate_samples(vectors, corpus, vec_size):
+    word_dict = parse_word_vecs(vectors, vec_size)
+    sentences = parse_data(corpus)
+    sentence_dict = {}
+    for sentence in sentences:
+        res = get_vecs_from_sentence(sentence, word_dict)
+        if res is not None:
+            sentence_dict[sentence] = res
+    return sentence_dict
 
 def get_vecs_from_sentence(sentence, word_dict):
     temp = []
 
     for word in re.findall(r"[\w]+|[^\s\w]", sentence):
-        curr = word_dict.get(word.lower())
+        curr = word_dict.get(word)
 
         if curr is None:
             return None
@@ -40,7 +50,7 @@ def parse_word_vecs(vec_path, vec_dim):
         i += 1
 
         if i % 100000 == 0:
-            break
+           break
 
     in_file.close()
 
@@ -60,6 +70,4 @@ if __name__ == "__main__":
     nltk.download('punkt')
     args = parse_args()
 
-    sentences = parse_data(args.training_file)
-    dict = parse_word_vecs(args.vec_file, VEC_DIM)
-    print(len(list(dict.keys())))
+    sentence_dict = generate_samples(args.vec_file, args.training_file, args.vec_dim)
