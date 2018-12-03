8606ea266d719b1995d40e28ca3991b74e393cd3
Wally Guzman <gualbertoguzman@gmail.com>
2018-12-03 12:34:37 -0600

    Changing to PCFG for 4 files

--- condor/raam.sh
+++ condor/raam.sh
@@ -11,26 +11,40 @@ for size in ${HIDDEN_SIZES[*]}; do
     echo "Testing with size:" $size "on random data";
     echo "python ../draam.py --hidden-size $size";
     echo;
+    touch ../logs/gram_1/random/once_$size.out;
     python ../draam.py --hidden-size $size --training-file ../data/gram_1/random/once00 \
         --test-file ../data/gram_1/random/once01 > ../logs/gram_1/random/once_$size.out;
     echo;
+    touch ../logs/gram_1/random/twice_$size.out;
     python ../draam.py --hidden-size $size --training-file ../data/gram_1/random/twice00 \
         --test-file ../data/gram_1/random/twice01 > ../logs/gram_1/random/twice_$size.out;
     echo;
+    touch ../logs/gram_1/random/thrice_$size.out;
     python ../draam.py --hidden-size $size --training-file ../data/gram_1/random/thrice00 \
         --test-file ../data/gram_1/random/thrice01 > ../logs/gram_1/random/thrice_$size.out;
     echo;
+    touch ../logs/gram_1/random/four_$size.out;
     python ../draam.py --hidden-size $size --training-file ../data/gram_1/random/four00 \
         --test-file ../data/gram_1/random/four01 > ../logs/gram_1/random/four_$size.out;
 done
 
 # Test on recursive data
-# for size in ${HIDDEN_SIZES[*]}; do
-#     python ../draam.py;
-#     echo;
-# done
-
-# Recursion tests: (train in gram_1/recurse, test in gram_1/only)
-#     1.  train:  recurse_once.txt,    test:  only_twice.txt
-#     2.  train:  recurse_twice.txt,   test:  only_thrice.txt
-#     3.  train:  recurse_thrice.txt,  test:  only_four.txt
+for size in ${HIDDEN_SIZES[*]}; do
+    python ../draam.py;
+    echo;
+    echo "Testing with size:" $size "on recursive data";
+    echo "python ../draam.py --hidden-size $size";
+    echo;
+    touch ../logs/gram_1/only/twice_$size.out;
+    python ../draam.py --hidden-size $size --training-file ../data/gram_1/random/four00 \
+    python ../draam.py --hidden-size $size --training-file ../data/gram_1/recurse/recurse_once.txt \
+        --test-file ../data/gram_1/only/only_twice.txt > ../logs/gram_1/only/twice_$size.out;
+    echo;
+    touch ../logs/gram_1/only/thrice_$size.out;
+    python ../draam.py --hidden-size $size --training-file ../data/gram_1/recurse/recurse_twice.txt \
+        --test-file ../data/gram_1/only/only_thrice.txt > ../logs/gram_1/only/thrice_$size.out;
+    echo;
+    touch ../logs/gram_1/only/four_$size.out;
+    python ../draam.py --hidden-size $size --training-file ../data/gram_1/recurse/recurse_thrice.txt \
+        --test-file ../data/gram_1/only/only_four.txt > ../logs/gram_1/only/four_$size.out;
+done
