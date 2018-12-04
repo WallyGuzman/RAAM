07d162c8ca9c5e79b9bc9e15849e12ea4e42974a
Wally Guzman <gualbertoguzman@gmail.com>
2018-12-04 17:54:11 -0600

    No redirect

--- condor/raam_small.sh
+++ condor/raam_small.sh
@@ -13,25 +13,17 @@ for hidden_size in ${HIDDEN_SIZES[*]}; do
         echo "Testing nested PPAs";
         echo "Testing with size:" $hidden_size "on random data:" $data_size;
         echo;
-        touch /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/random/"$hidden_size"_"$data_size".out;
-        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_"$data_size".txt > \ 
-            /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/small/random/"$hidden_size"_"$data_size".out;
+        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_"$data_size".txt;
         echo "Testing with size:" $hidden_size "on recursive data:" $data_size;
         echo;
-        touch /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/sorted/"$hidden_size"_"$data_size".out;
-        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_"$data_size"_sorted.txt > \ 
-            /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/small/sorted/"$hidden_size"_"$data_size".out;
+        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_"$data_size"_sorted.txt;
 
         echo "Testing nested SCs";
         echo "Testing with size:" $hidden_size "on random data:" $data_size;
         echo;
-        touch /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/random/"$hidden_size"_"$data_size"_subord.out;
-        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_subord_"$data_size".txt > \ 
-            /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/small/random/"$hidden_size"_"$data_size"_subord.out;
+        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_subord_"$data_size".txt;
         echo "Testing with size:" $hidden_size "on recursive data:" $data_size;
         echo;
-        touch /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/sorted/"$hidden_size"_"$data_size"_subord.out;
-        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_subord_"$data_size"_sorted.txt > \ 
-            /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/small/sorted/"$hidden_size"_"$data_size"_subord.out;
-        done;
+        python "$DRAAM" --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_subord_"$data_size"_sorted.txt;
+    done;
 done
