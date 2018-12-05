ef73e38c6c7085801f37fc75e071fc8c6f97e09a
Wally Guzman <gualbertoguzman@gmail.com>
2018-12-05 07:35:45 -0600

    Fix raam.sh script arg error and add results for one_hot tests

--- condor/raam.sh
+++ condor/raam.sh
@@ -3,20 +3,27 @@
 # shell script to execute Python jobs in vanilla universe
 #
 
-HIDDEN_SIZES=(300 600 1200 2400 4800 9600)
-DATA_SIZES=(125000 250000 500000 1000000)
-# NOTE: Testing with DRAAM only, for now
+HIDDEN_SIZES=(100 200 300)
+DATA_SIZES=(500 1000 2000 4000 8000)
+# NOTE: Testing with one_hot_raam only, for now
+DRAAM=/u/gguzman/CS-394N/Final-Project/RAAM/one_hot_raam.py
 
-# Test on random data
 for hidden_size in ${HIDDEN_SIZES[*]}; do
     for data_size in ${DATA_SIZES[*]}; do
+        echo "Testing nested PPAs";
         echo "Testing with size:" $hidden_size "on random data:" $data_size;
         echo;
-        touch /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/random/"$hidden_size"_"$data_size".out
-        python /u/gguzman/CS-394N/Final-Project/RAAM/draam.py --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/gram_4/gram_4_"$data_size".txt > /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/random/"$hidden_size"_"$data_size".out;
+        python "$DRAAM" --hidden-size $hidden_size --grammar-file /u/gguzman/CS-394N/Final-Project/RAAM/grammars/gram_file_4.txt --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_"$data_size".txt;
         echo "Testing with size:" $hidden_size "on recursive data:" $data_size;
         echo;
-        touch /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/sorted/"$hidden_size"_"$data_size".out
-        python /u/gguzman/CS-394N/Final-Project/RAAM/draam.py --hidden-size $hidden_size --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/gram_4/gram_4_"$data_size"_sorted.txt > /u/gguzman/CS-394N/Final-Project/RAAM/logs/gram_4/sorted/"$hidden_size"_"$data_size".out;
-        done;
+        python "$DRAAM" --hidden-size $hidden_size --grammar-file /u/gguzman/CS-394N/Final-Project/RAAM/grammars/gram_file_4.txt --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_"$data_size"_sorted.txt;
+
+        echo "Testing nested SCs";
+        echo "Testing with size:" $hidden_size "on random data:" $data_size;
+        echo;
+        python "$DRAAM" --hidden-size $hidden_size --grammar-file /u/gguzman/CS-394N/Final-Project/RAAM/grammars/gram_file_subord_4.txt --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_subord_"$data_size".txt;
+        echo "Testing with size:" $hidden_size "on recursive data:" $data_size;
+        echo;
+        python "$DRAAM" --hidden-size $hidden_size --grammar-file /u/gguzman/CS-394N/Final-Project/RAAM/grammars/gram_file_subord_4.txt --training-file /u/gguzman/CS-394N/Final-Project/RAAM/data/small/gram_4_subord_"$data_size"_sorted.txt;
+    done;
 done
